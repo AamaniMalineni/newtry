dd40746e9a9c1322d7f466eb1f7f8d5150ecd137
AamaniMalineni <50495275+AamaniMalineni@users.noreply.github.com>
2021-10-21 10:24:48 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,7 @@
+import java.util;
+class Student{
+	public static void main(String args){
+		int a=10;
+                String s="hi";x
+		System.out.println("second branch");
+                System.out.println("third branch");
